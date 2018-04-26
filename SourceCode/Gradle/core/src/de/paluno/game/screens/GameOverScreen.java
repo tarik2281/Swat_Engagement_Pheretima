b3ab8ab85966cb70d00f46aaf70092da9ad1565e
Ibrahim Kekec <ibrahim.kekec@stud.uni-due.de>
2018-04-26 18:44:02 +0200

    Klasse erstellt

---
+++ SourceCode/Gradle/core/src/de/paluno/game/screens/GameOverScreen.java
@@ -0,0 +1,22 @@
+package de.paluno.game.screens;
+
+import de.paluno.game.SEPGame;
+
+public class GameOverScreen {
+	
+	public GameOverScreen(SEPGame game, WinningPlayer winplayer) {
+		
+	}
+	
+	public void show() {
+		
+	}
+	
+	public void render(float delta) {
+		
+	}
+	
+	public void hide() {
+		
+	}
+}
