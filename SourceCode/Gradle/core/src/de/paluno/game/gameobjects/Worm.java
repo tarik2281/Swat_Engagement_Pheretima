566d85bc82363fe23c3a2adc68f1b436adc36fa1
Jan <jan.voigtlaender@stud.uni-due.de>
2018-04-26 23:57:22 +0200

    + setupBody() weitergeführt + render() begonnen

--- SourceCode/Gradle/core/src/de/paluno/game/gameobjects/Worm.java
+++ SourceCode/Gradle/core/src/de/paluno/game/gameobjects/Worm.java
@@ -1,7 +1,10 @@
 package de.paluno.game.gameobjects;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.*;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 
 import de.paluno.game.GameState;
 import de.paluno.game.screens.PlayScreen;
@@ -12,22 +15,49 @@ public class Worm implements Updatable, PhysicsObject, Renderable {
 	private int playerNumber;
 	
 	private Body body;
+	private Fixture fixture;
 	
 	public Worm(int num, PlayScreen screen) {
 		this.playerNumber = num;
 		this.screen = screen;
+		
+		this.setupBody();
 	}
 	
 	public void update(float delta, GameState state) {
-		
+		if(this.body == null) return;
 	}
 	
 	public void render(SpriteBatch batch, float delta) {
+		if(this.body == null) return;
 		
+		Texture texture = new Texture(Gdx.files.internal("waccuse1_blank.png"));
+		// TODO: Rename textures. 
+		
+		batch.begin();
+		batch.draw(texture, 10, 10);
+		batch.end();
 	}
 	
 	public void setupBody() {
-		this.body = new Body(this.screen.getWorld(), addr);
+		BodyDef bodyDef = new BodyDef();
+		bodyDef.position.set(10, 50);
+		bodyDef.type = BodyType.DynamicBody;
+		
+		this.body = this.screen.getWorld().createBody(bodyDef);
+		
+		PolygonShape bodyRect = new PolygonShape();
+		bodyRect.setAsBox(5, 10);
+		
+		FixtureDef fixtureDef = new FixtureDef();
+		fixtureDef.shape = bodyRect;
+		fixtureDef.density = 0.5f;
+		fixtureDef.friction = 0.8f;
+		fixtureDef.restitution = 0.2f;
+		
+		this.fixture = this.body.createFixture(fixtureDef);
+		
+		bodyRect.dispose();
 	}
 	
 	public Body getBody() {
