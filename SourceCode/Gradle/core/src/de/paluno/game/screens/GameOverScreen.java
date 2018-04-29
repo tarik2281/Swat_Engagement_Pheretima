b8ffab92fda9a4772d1f914338abd0c5eedd32a2
Ibrahim Kekec <ibrahim.kekec@stud.uni-due.de>
2018-04-29 17:20:17 +0200

    GameOverScreen - erstmal "fertig"

--- SourceCode/Gradle/core/src/de/paluno/game/screens/GameOverScreen.java
+++ SourceCode/Gradle/core/src/de/paluno/game/screens/GameOverScreen.java
@@ -1,37 +1,54 @@
 package de.paluno.game.screens;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
 import de.paluno.game.SEPGame;
 
 public class GameOverScreen extends com.badlogic.gdx.ScreenAdapter {
 	
 	protected Sprite sprite;
-	private SEPGame game;
+	private SpriteBatch batch;
+	private Texture sp1won;
+	private Texture sp2won;
 	private WinningPlayer winningPlayer;
 	
 	public GameOverScreen(SEPGame game, WinningPlayer winningPlayer) {
 		super();
-		this.game = game;
 		this.winningPlayer = winningPlayer;
 	}
 	
 	public void show() {
 		sprite = new Sprite();
+		batch = new SpriteBatch();
 		
 		if (winningPlayer == WinningPlayer.PLAYERONE) {
-			new Texture(("Spieler1Win"));
+			sp1won = new Texture(Gdx.files.internal("Spieler1Win.png"));
 		}else{
-			new Texture(("Spieler2Win"));
+			sp2won = new Texture(Gdx.files.internal("Spieler2Win.png"));
 		}
 	}
 	
 	public void render(float delta) {
-		
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		if (winningPlayer == WinningPlayer.PLAYERONE) {
+			batch.draw(sp1won, 0, 0);
+		}else{
+			batch.draw(sp2won, 0, 0);
+		}
+		batch.end();
 	}
 	
-	public void hide() {
-		
+	public void hide(){
+		batch.dispose();
+		if(winningPlayer == WinningPlayer.PLAYERONE){
+			sp1won.dispose();
+		}else{
+			sp2won.dispose();
+		}
 	}
 }
