b5005a8ecc88588efe27a0fea42c7deb087bcc68
Tarik Karaca <tarik.karaca@stud.uni-duisburg-essen.de>
2018-05-03 18:12:27 +0200

    PlayScreen, Projectile

--- SourceCode/Gradle/core/src/de/paluno/game/gameobjects/Projectile.java
+++ SourceCode/Gradle/core/src/de/paluno/game/gameobjects/Projectile.java
@@ -1,12 +1,20 @@
 package de.paluno.game.gameobjects;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.*;
+import de.paluno.game.Constants;
 import de.paluno.game.GameState;
 import de.paluno.game.screens.PlayScreen;
 
 public class Projectile implements Updatable, PhysicsObject, Renderable {
+
+    // in meters
+    private static final float PROJECTILE_RADIUS = 3.0f;
+    private static final float PROJECTILE_DENSITY = 10.0f;
 
     private PlayScreen playScreen;
     private Vector2 origin;
@@ -14,10 +22,18 @@ public class Projectile implements Updatable, PhysicsObject, Renderable {
 
     private Body body;
 
+    private Texture texture;
+    private Sprite sprite;
+
     public Projectile(PlayScreen playScreen, Vector2 origin, Vector2 direction) {
         this.playScreen = playScreen;
         this.origin = origin;
         this.direction = direction;
+
+        texture = new Texture(Gdx.files.internal("Projectile.png"));
+        sprite = new Sprite(texture);
+
+        sprite.setOriginCenter();
     }
 
     @Override
@@ -27,12 +43,39 @@ public class Projectile implements Updatable, PhysicsObject, Renderable {
 
     @Override
     public void render(SpriteBatch batch, float delta) {
+        Vector2 position = Constants.getScreenSpaceVector(body.getPosition());
 
+        sprite.setOriginBasedPosition(position.x, position.y);
+        sprite.draw(batch);
     }
 
     @Override
     public void setupBody() {
+        World world = playScreen.getWorld();
 
+        BodyDef bodyDef = new BodyDef();
+        bodyDef.type = BodyDef.BodyType.DynamicBody;
+        Vector2 position = Constants.getWorldSpaceVector(origin);
+        bodyDef.position.set(position.x, position.y);
+        bodyDef.bullet = true;
+
+        CircleShape shape = new CircleShape();
+        shape.setRadius(PROJECTILE_RADIUS * Constants.WORLD_SCALE);
+
+        FixtureDef fixtureDef = new FixtureDef();
+        fixtureDef.shape = shape;
+        fixtureDef.density = PROJECTILE_DENSITY;
+
+        body = world.createBody(bodyDef);
+
+        body.createFixture(fixtureDef);
+
+        body.setGravityScale(0.0f);
+        body.setUserData(this);
+
+        body.applyLinearImpulse(direction, body.getPosition(), true);
+
+        shape.dispose();
     }
 
     @Override
--- SourceCode/Gradle/core/src/de/paluno/game/screens/PlayScreen.java
+++ SourceCode/Gradle/core/src/de/paluno/game/screens/PlayScreen.java
@@ -3,13 +3,17 @@ package de.paluno.game.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.World;
+import de.paluno.game.CollisionHandler;
 import de.paluno.game.Constants;
 import de.paluno.game.GameState;
 import de.paluno.game.SEPGame;
-import de.paluno.game.gameobjects.PhysicsObject;
-import de.paluno.game.gameobjects.Updatable;
+import de.paluno.game.gameobjects.*;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
@@ -29,8 +33,45 @@ public class PlayScreen extends ScreenAdapter {
     private ArrayList<Renderable> renderableObjects;
     private ArrayList<Updatable> updatableObjects;
 
+    private Worm[] playerWorms;
+
+    private CollisionHandler collisionHandler;
+
+    private OrthographicCamera camera;
+    private PhysicsObject cameraFocus;
+
+    private Box2DDebugRenderer debugRenderer;
+    private OrthographicCamera debugCamera;
+
     private InputAdapter inputAdapter = new InputAdapter() {
         // TODO: input handling
+
+        @Override
+        public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+            // mouse button down
+            return super.touchDown(screenX, screenY, pointer, button);
+        }
+
+        @Override
+        public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+            // mouse button up
+            return super.touchUp(screenX, screenY, pointer, button);
+        }
+
+        @Override
+        public boolean mouseMoved(int screenX, int screenY) {
+            return super.mouseMoved(screenX, screenY);
+        }
+
+        @Override
+        public boolean keyUp(int keycode) {
+            return super.keyUp(keycode);
+        }
+
+        @Override
+        public boolean keyDown(int keycode) {
+            return super.keyDown(keycode);
+        }
     };
 
     public PlayScreen(SEPGame game) {
@@ -46,17 +87,36 @@ public class PlayScreen extends ScreenAdapter {
 
         renderableObjects = new ArrayList<Renderable>();
         updatableObjects = new ArrayList<Updatable>();
+
+        playerWorms = new Worm[Constants.NUM_PLAYERS];
     }
 
     @Override
     public void show() {
         Gdx.input.setInputProcessor(inputAdapter);
+
+        registerAfterUpdate(new Projectile(this, new Vector2(200, 200), new Vector2(1.0f, 0.0f)));
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
+        debugRenderer = new Box2DDebugRenderer();
+        debugCamera = new OrthographicCamera();
+
+        float debugWidth = Gdx.graphics.getWidth() * Constants.WORLD_SCALE;
+        float debugHeight = Gdx.graphics.getHeight() * Constants.WORLD_SCALE;
+
+        debugCamera.setToOrtho(false, debugWidth, debugHeight);
+        debugCamera.lookAt(debugWidth / 2, debugHeight / 2, 0);
+        debugCamera.update();
     }
 
     @Override
     public void render(float delta) {
         // game loop
         registerObjects();
+
+        Gdx.graphics.setTitle("SEPGame FPS: " + Gdx.graphics.getFramesPerSecond());
 
         updatePhase(delta);
         physicsPhase(delta);
@@ -71,14 +131,32 @@ public class PlayScreen extends ScreenAdapter {
     }
 
     public void physicsPhase(float delta) {
+        float timeStep = 1.0f / 60.0f;
         // TODO: constant time step for consistent physics simulation
-        world.step(delta, Constants.VELOCITY_ITERATIONS, Constants.POSITION_ITERATIONS);
+        world.step(timeStep, Constants.VELOCITY_ITERATIONS, Constants.POSITION_ITERATIONS);
     }
 
     public void renderPhase(float delta) {
+        Gdx.gl.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        debugRenderer.render(world, debugCamera.combined);
+
+        if (cameraFocus != null) {
+            Vector2 position = Constants.getScreenSpaceVector(cameraFocus.getBody().getPosition());
+            camera.lookAt(position.x, position.y, 0);
+            camera.update();
+        }
+
+        spriteBatch.begin();
+        spriteBatch.setProjectionMatrix(camera.projection);
+        spriteBatch.setTransformMatrix(camera.view);
+
         for (Renderable renderable : renderableObjects) {
             renderable.render(spriteBatch, delta);
         }
+
+        spriteBatch.end();
     }
 
     public void updatePhase(float delta) {
@@ -98,7 +176,7 @@ public class PlayScreen extends ScreenAdapter {
     }
 
     public void setCameraFocus(PhysicsObject gameObject) {
-
+        cameraFocus = gameObject;
     }
 
     public World getWorld() {
@@ -107,6 +185,10 @@ public class PlayScreen extends ScreenAdapter {
 
     public void setGameState(GameState gameState) {
         this.gameState = gameState;
+    }
+
+    public GameState getGameState() {
+        return this.gameState;
     }
 
     public void advanceGameState() {
@@ -127,6 +209,8 @@ public class PlayScreen extends ScreenAdapter {
                 renderableObjects.add((Renderable)gameObject);
             }
         }
+
+        objectRegisterQueue.clear();
     }
 
     private void forgetObjects() {
@@ -143,6 +227,26 @@ public class PlayScreen extends ScreenAdapter {
             if (gameObject instanceof Renderable) {
                 renderableObjects.remove((Renderable)gameObject);
             }
+
+            if (gameObject == cameraFocus)
+                setCameraFocus(null);
         }
+
+        objectForgetQueue.clear();
+    }
+
+    private int getCurrentPlayer() {
+        int player = -1;
+
+        switch (gameState) {
+            case PLAYERONETURN:
+                player = 0;
+                break;
+            case PLAYERTWOTURN:
+                player = 1;
+                break;
+        }
+
+        return player;
     }
 }
