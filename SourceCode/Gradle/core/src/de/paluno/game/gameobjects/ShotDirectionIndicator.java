4d02209bce02234dd4cd977721481285b8f40c43
Ibrahim Kekec <ibrahim.kekec@stud.uni-due.de>
2018-05-04 15:16:39 +0200

    ShotDirectionIndicator überarbeitet

--- SourceCode/Gradle/core/src/de/paluno/game/gameobjects/ShotDirectionIndicator.java
+++ SourceCode/Gradle/core/src/de/paluno/game/gameobjects/ShotDirectionIndicator.java
@@ -3,6 +3,8 @@ package de.paluno.game.gameobjects;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.physics.box2d.Body;
 
@@ -11,30 +13,49 @@ import de.paluno.game.screens.PlayScreen;
 
 public class ShotDirectionIndicator extends java.lang.Object implements Renderable, Updatable{
 	
+	private PlayScreen playScreen;
 	private Worm worm;
 	private int playerNumber;
+	private float positionX;
+	private float positionY;
+	private float mousePositionX;
+	private float mousePositionY;
+	private Sprite sprite;
+	private Sprite sprite2;
 	private SpriteBatch batch;
 	private GameState gamestate;
-	private Texture arrow;
+	private Texture texture;
+	private Texture texture2;
+	
 	
 	public ShotDirectionIndicator(int playerNumber, Worm worm, PlayScreen playScreen) {
-		super();
-		this.worm = worm;
 		this.playerNumber = playerNumber;
-		arrow = new Texture(Gdx.files.internal("Pfeil.png"));
-	}
-
-	public void render(SpriteBatch batch, float delta){
-		this.batch = batch;
-		if(Gdx.input.isKeyJustPressed(Keys.SPACE) == true){
-			batch.begin();
-			batch.draw(arrow, 
-		}
+		this.worm = worm;
+		this.playScreen = playScreen;
+		
+		texture = new Texture(Gdx.files.internal("Projectile.png"));
+		texture2 = new Texture(Gdx.files.internal("Pfeil.png"));
+		sprite = new Sprite(texture);
+		sprite2 = new Sprite(texture2);
 		
 	}
-	
+
 	public void update(float delta, GameState gamestate) {
 		this.gamestate = gamestate;
 	}
 	
+	public void render(SpriteBatch batch, float delta){
+		positionX = worm.getBody().getPosition().x;
+	    positionY = worm.getBody().getPosition().y;
+		
+	//	mousePositionX = 0;
+	//	mousePositionY = 0;
+		
+		batch.begin();
+		batch.draw(texture, positionX + 3, positionY + 3);
+		batch.end();
+		
+		}
+		
+	}
 }
