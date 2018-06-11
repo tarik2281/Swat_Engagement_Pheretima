d6bb0f63fcc13d56107b6ec285087e9cfb1bb31a
Ibrahim Kekec <ibrahim.kekec@stud.uni-due.de>
2018-06-11 14:57:05 +0200

    Modultest - Player

---
+++ SourceCode/Gradle/core/src/de/paluno/game/test/PlayerTest.java
@@ -0,0 +1,20 @@
+package de.paluno.game.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+
+class PlayerTest {
+	
+	@Before
+	public void setUp() {
+		
+	}
+	
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
