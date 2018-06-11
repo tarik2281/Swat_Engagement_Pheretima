ddb1b89d721e84414eb57837378c969f9fbffcb8
Ibrahim Kekec <ibrahim.kekec@stud.uni-due.de>
2018-06-11 17:24:12 +0200

    Test Ordner erstellt für den Modultest

---
+++ SourceCode/Gradle/core/test/de/paluno/game/test/PlayerTest.java
@@ -0,0 +1,24 @@
+package de.paluno.game.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PlayerTest {
+
+	@Test
+	public void testSetTurn() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testShiftTurn() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCharacterDied() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ SourceCode/Gradle/core/test/de/paluno/game/test/WorldTest.java
@@ -0,0 +1,26 @@
+package de.paluno.game.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+
+class WorldTest {
+	
+	Player player_mock;
+	
+	@Before
+	public void setUp() {
+		
+	}
+	@Test
+	void testAdvanceGameState() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGenerateSpawnPosition() {
+		fail("Not yet implemented");
+	}
+
+}
